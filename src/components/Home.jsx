04c2a849d9c2e64dbf7a315499c4c54918d81b5b
Shriviyaas <shriviyaas2@gmail.com>
2024-01-05 14:29:16 +0530

    Style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,10 @@ const Home = () => {
       <p className='phome'>
         Explore our wide range of products and enjoy a seamless shopping experience.
       </p>
+
+      <p className='phone'>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti optio omnis vero a obcaecati error excepturi sapiente! Dolorem deleniti nisi impedit facilis eum consequatur non, architecto eos sapiente sunt nihil!
+      </p>
     </div>
   );
 };
